e3180f52739fb65d0d28cb6bceecc6eed76d2c8d
Alex Braha Stoll <alexbrahastoll@gmail.com>
2019-08-11 12:50:40 -0300

    Initial commit

---
+++ lib/oasis.ex
@@ -0,0 +1,18 @@
+defmodule Oasis do
+  @moduledoc """
+  Documentation for Oasis.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oasis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
