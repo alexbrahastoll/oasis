1e960c9388c291bb01d3b304be2d516dd2f226a5
Alex Braha Stoll <alexbrahastoll@gmail.com>
2019-08-29 12:12:26 -0300

    Adds heuristic for detecting the sequential ID antipattern

--- lib/oasis/antipattern.ex
+++ lib/oasis/antipattern.ex
@@ -5,7 +5,8 @@ defmodule Oasis.Antipattern do
   defstruct [:violator, :type]
 
   def detect_all(yaml) do
-    deep_paths(yaml)
+    [deep_paths(yaml), sequential_id(yaml)]
+    |> List.flatten()
   end
 
   defp deep_paths(yaml) do
@@ -16,6 +17,41 @@ defmodule Oasis.Antipattern do
         %Antipattern{violator: "paths.#{path}", type: :deep_path}
       end
     end)
+    |> Enum.filter(& &1)
+  end
+
+  defp sequential_id(yaml) do
+    yaml["paths"]
+    |> Map.keys()
+    |> Enum.map(fn path ->
+      path_metadata = yaml["paths"][path]
+      get_metadata = path_metadata["get"]
+
+      if get_metadata do
+        relevant_params =
+          Regex.scan(~r/\{((?:[a-z]|[0-9]|[_-])+)\}/i, path)
+          |> List.flatten()
+          |> Enum.reject(fn match -> String.starts_with?(match, "{") end)
+          |> Enum.filter(fn param ->
+            String.match?(param, ~r/(?:.+[_-]*[iI](?:d|D|dentifier))$/)
+          end)
+
+        (get_metadata["parameters"] ||
+           [])
+        |> Enum.filter(fn param_metadata ->
+          Enum.member?(relevant_params, param_metadata["name"])
+        end)
+        |> Enum.map(fn param_metadata ->
+          if (param_metadata["schema"] || %{})["type"] == "integer" do
+            %Antipattern{
+              violator: "paths.#{path}?#{param_metadata["name"]}",
+              type: :sequential_id
+            }
+          end
+        end)
+      end
+    end)
+    |> List.flatten()
     |> Enum.filter(& &1)
   end
 end
